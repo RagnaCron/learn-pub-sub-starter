d8a02c2a3983a6357c1c450fa2d484e80f22f699
Manuel Werder <manuelwerder@hotmail.com>
2026-03-15 14:20:08 +0100

    Running the server with some commands

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,12 +6,15 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/RagnaCron/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/RagnaCron/learn-pub-sub-starter/internal/pubsub"
 	"github.com/RagnaCron/learn-pub-sub-starter/internal/routing"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	gamelogic.PrintServerHelp()
 	const connectionString = "amqp://guest:guest@localhost:5672/"
 
 	con, err := amqp.Dial(connectionString)
@@ -25,9 +28,29 @@ func main() {
 		log.Fatalf("could not create channel for RabbitMQ: %v\n", err)
 	}
 
-	err = pubsub.PublishJSON(chann, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
-	if err != nil {
-		log.Fatalf("could not PublishJSON: %v\n", err)
+	for {
+		input := gamelogic.GetInput()
+		com := input[0]
+
+		if com == "pause" {
+			log.Println("Pausing the game.")
+			err = pubsub.PublishJSON(chann, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+			if err != nil {
+				log.Fatalf("could not PublishJSON: %v\n", err)
+			}
+		} else if com == "resume" {
+			log.Println("Resuming the game.")
+			err = pubsub.PublishJSON(chann, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
+			if err != nil {
+				log.Fatalf("could not PublishJSON: %v\n", err)
+			}
+
+		} else if com == "quit" {
+			log.Println("Exiting the game.")
+			break
+		} else {
+			log.Printf("Unkown command: %v\n", com)
+		}
 	}
 
 	fmt.Println("Peril game server connected to RabbitMQ")
